626ea5f4eb9d467193bc6848bc25ef24be1f7e5b
Pierre Rouanet <pierre.rouanet@gmail.com>
2020-05-27 10:44:02 +0200

    Add fan driver.

---
+++ Drivers/Fan/fan.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include "fan.h"
+
+
+module_t* fans[3];
+
+void rx_fan_cb(module_t *module, msg_t *msg) {
+    if (msg->header.cmd == IO_STATE) {
+        if (module == fans[0]) {
+            HAL_GPIO_WritePin(SHOULDER_FAN_GPIO_Port, SHOULDER_FAN_Pin, msg->data[0]);
+        }
+        if (module == fans[1]) {
+            HAL_GPIO_WritePin(ELBOW_FAN_GPIO_Port, ELBOW_FAN_Pin, msg->data[0]);
+        }
+        if (module == fans[2]) {
+            HAL_GPIO_WritePin(WRIST_FAN_GPIO_Port, WRIST_FAN_Pin, msg->data[0]);
+        }
+        
+    }
+}
+
+void fan_init(void) {
+    fans[0] = luos_module_create(rx_fan_cb, STATE_MOD, "shoulder_fan");
+    fans[1] = luos_module_create(rx_fan_cb, STATE_MOD, "elbow_fan");
+    fans[2] = luos_module_create(rx_fan_cb, STATE_MOD, "wrist_fan");
+    luos_module_enable_rt(fans[0]);
+    luos_module_enable_rt(fans[1]);
+    luos_module_enable_rt(fans[2]);
+
+    // ********************* Gpio configuration ****************************
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    // GPIO Ports Clock Enable
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    // Set a safe pin state just in case
+    HAL_GPIO_WritePin(SHOULDER_FAN_GPIO_Port, SHOULDER_FAN_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(ELBOW_FAN_GPIO_Port, ELBOW_FAN_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(WRIST_FAN_GPIO_Port, WRIST_FAN_Pin, GPIO_PIN_RESET);
+    // configure your pin using the GPIO_InitTypeDef structure
+    GPIO_InitStruct.Pin = SHOULDER_FAN_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(SHOULDER_FAN_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = ELBOW_FAN_Pin;
+    HAL_GPIO_Init(ELBOW_FAN_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = WRIST_FAN_Pin;
+    HAL_GPIO_Init(WRIST_FAN_GPIO_Port, &GPIO_InitStruct);
+}
+
+void fan_loop(void) {
+}
