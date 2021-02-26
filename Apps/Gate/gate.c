50b90071bae18586c20a9c427036be8bb565b0fd
Pierre Rouanet <pierre.rouanet@gmail.com>
2021-02-26 11:50:56 +0100

    Increase recv buff size to avoid #24

--- Apps/Gate/gate.c
+++ Apps/Gate/gate.c
@@ -6,7 +6,7 @@
 #define SEND_BUFF_SIZE 64
 uint8_t send_buff[SEND_BUFF_SIZE] = {0};
 
-#define RECV_BUFF_SIZE 64
+#define RECV_BUFF_SIZE 256
 #define RECV_RING_BUFFER_SIZE 15
 volatile uint8_t recv_buff[RECV_RING_BUFFER_SIZE][RECV_BUFF_SIZE] = {0};
 volatile uint8_t recv_buff_msg_size[RECV_RING_BUFFER_SIZE] = {0};
